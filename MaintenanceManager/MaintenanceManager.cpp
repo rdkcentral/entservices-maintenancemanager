cfa76ac2d9688ded4430f03f043c0798cecfdbb6
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-04-07 19:53:35 +0530

    Update MaintenanceManager.cpp

--- MaintenanceManager/MaintenanceManager.cpp
+++ MaintenanceManager/MaintenanceManager.cpp
@@ -306,7 +306,7 @@ namespace WPEFramework
             "RFCbase.sh",
 #endif
             "rdkvfwupgrader",
-            "uploadSTBLogs.sh"
+            "logupload"
         };
 
         static const array<string, 3> kDeviceInitContextKeyVals = {
