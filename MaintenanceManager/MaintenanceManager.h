f9c8ecf02c3922183d85d370a139b23d9326eefe
Saranya <saranya_elango@comcast.com>
2026-06-08 05:03:38 +0000

    Fix for build error

--- MaintenanceManager/MaintenanceManager.h
+++ MaintenanceManager/MaintenanceManager.h
@@ -254,7 +254,7 @@ namespace WPEFramework
             bool queryIAuthService();
             int abortTask(const char *, int sig = SIGABRT);
             pid_t getTaskPID(const char *);
-	    bool skipUnsolicitedMaintenanceAtBoot(const string &lastRebootReason);
+            bool skipUnsolicitedMaintenanceAtBoot(const string &lastRebootReason, const string &lastMaintenanceStatus);
             string getLastRebootReason();
             void iarmEventHandler(const char *owner, IARM_EventId_t eventId, void *data, size_t len);
             static void _MaintenanceMgrEventHandler(const char *owner, IARM_EventId_t eventId, void *data, size_t len);
